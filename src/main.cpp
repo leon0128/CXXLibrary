7e75a403c1743afd851b90db60245db6ad25ffe2
leon0128 <leon0128>
2019-11-12 12:43:22 +0900

    [update] Implementation of bool_constant struct

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,7 @@
 
 int main(int argc, char** argv)
 {
-    using int_zero = std::integral_constant<int, 0>;
-
-    using int_Zero = LEON::integral_constant<int, 0>;
-
-    std::cout << "std::typeid:   " << typeid(int_zero::value_type()).name() << std::endl;
-    std::cout << "LEON::typeid: " << typeid(int_Zero::value_type()).name() << std::endl;
+    std::cout << LEON::bool_constant<true>::value << std::endl;
 
     return 0;
 }
--- src/type_traits.hpp
+++ src/type_traits.hpp
@@ -1,6 +1,7 @@
 namespace LEON
 {
 
+// 整数定数を表す構造体
 template<typename T, T v>
 struct integral_constant
 {
@@ -14,5 +15,11 @@ struct integral_constant
     // c++14
     constexpr value_type operator()() const noexcept {return value;}
 };
+
+// 真理値型の定数を表す構造体
+template<bool B>
+using bool_constant = integral_constant<bool, B>;
+
+
 
 };
