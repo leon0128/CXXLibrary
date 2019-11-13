6c6b25ff228066ff795c8d674acf21666bb0410a
leon0128 <leon0128>
2019-11-13 10:32:32 +0900

    [update] Implementation of cstddef.hpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,28 @@
 #include <type_traits>
 #include <typeinfo>
 #include <iostream>
+#include <cstddef>
 
 static_assert(std::is_same<LEON::remove_cv<const volatile int>::type,
                            int>::value, "foge");
 
+
+struct Str
+{
+    int x;
+    int y;
+    int z;
+};
+
 int main(int argc, char** argv)
 {
-    std::cout << typeid(std::is_void<const void>::type).name() << std::endl;
-    std::cout << typeid(std::true_type::type).name() << std::endl;
+    std::max_align_t align;
+    LEON::max_align_t align2;
+    LEON::byte a;
+
+    std::cout << offsetof(Str, y) << std::endl;
+    std::cout << typeid(int).name() << std::endl;
+    std::cout << typeid(LEON::byte).name() << std::endl;
 
     std::cout << LEON::is_void<void>::value << std::endl;
     std::cout << LEON::is_void<const void>::value << std::endl;
